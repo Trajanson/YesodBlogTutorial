575f328a2be0d616c609a2d621c1f6baf4823b19
Julian Trajanson <julian.trajanson@gmail.com>
2016-05-12 20:26:25 -0400

    Finished Tutorial

--- myBlog/Handler/Home.hs
+++ myBlog/Handler/Home.hs
@@ -14,33 +14,39 @@ import Text.Julius (RawJS (..))
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+  allPosts <- runDB $ selectList [] [Desc BlogPostId]
+  defaultLayout $ do
+    $(widgetFile "posts/index")
 
-postHomeR :: Handler Html
-postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
 
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField (withSmallInput "What's on the file?") Nothing
-
-commentIds :: (Text, Text, Text)
-commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
+--     (formWidget, formEnctype) <- generateFormPost sampleForm
+--     let submission = Nothing :: Maybe (FileInfo, Text)
+--         handlerName = "getHomeR" :: Text
+--     defaultLayout $ do
+--         let (commentFormId, commentTextareaId, commentListId) = commentIds
+--         aDomId <- newIdent
+--         setTitle "Welcome To Yesod!"
+--         $(widgetFile "homepage")
+--
+-- postHomeR :: Handler Html
+-- postHomeR = do
+--     ((result, formWidget), formEnctype) <- runFormPost sampleForm
+--     let handlerName = "postHomeR" :: Text
+--         submission = case result of
+--             FormSuccess res -> Just res
+--             _ -> Nothing
+--
+--     defaultLayout $ do
+--         let (commentFormId, commentTextareaId, commentListId) = commentIds
+--         aDomId <- newIdent
+--         setTitle "Welcome To Yesod!"
+--         $(widgetFile "homepage")
+--
+-- sampleForm :: Form (FileInfo, Text)
+-- sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
+--     <$> fileAFormReq "Choose a file"
+--     <*> areq textField (withSmallInput "What's on the file?") Nothing
+--
+-- commentIds :: (Text, Text, Text)
+-- commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
